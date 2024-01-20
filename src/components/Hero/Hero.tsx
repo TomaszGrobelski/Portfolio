26d6195d1afd602e68d2ac7f5f7ec5adb5083a1e
Tomasz <bleniog@interia.pl>
2024-01-20 12:59:34 +0100

    Added sidebar to Hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,3 +1,4 @@
+import { Icon } from '@iconify/react';
 import { forwardRef } from 'react';
 
 import AuthorImage from '../../assets/415774969_1379046102703725_3244739974429827889_n.jpg';
@@ -24,6 +25,17 @@ const Hero = forwardRef<HTMLSelectElement>((_, ref) => {
         </div>
       </div>
       <span className='welcom'>WELCOME</span>
+      <div className='social-media'>
+        <div className='social-box'>
+          <a href='https://www.linkedin.com/in/tomasz-grobelski-6182b4145/'>
+            <Icon icon='devicon:linkedin' width={28} />
+          </a>
+          <a href='https://github.com/TomaszGrobelski'>
+            <Icon icon='jam:github' color='white' width={28} />
+          </a>
+        </div>
+        <p className='web-dev'>Web Developer</p>
+      </div>
     </section>
   );
 });
