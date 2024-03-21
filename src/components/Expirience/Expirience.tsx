bcbff78c63b24d9b038a80a13797be88614ba65f
Tomasz <bleniog@interia.pl>
2024-03-21 14:48:34 +0100

    Portfolio: Update expirience description

--- src/components/Expirience/Expirience.tsx
+++ src/components/Expirience/Expirience.tsx
@@ -10,14 +10,43 @@ const Expirience = () => {
         <p className='role'>Role: Frontend Developer</p>
         <p className='duration'>Duration: 01.11.2023 - Present</p>
         <img src={adminPanel} alt='admin-panel' />
-        <p className='description'>
-          As a developer within a team dedicated to a social media application, my primary focus involved overseeing the
-          administrative panel. I designed a user-friendly table for displaying information, incorporating filters,
-          sorting, events handling such as changes in user status,pagination to streamline data navigation and created modals. I
-          collaborated closely with the team, gaining valuable experience and refining my Git version control skills.
-          Notably, during this collaborative effort, I received constructive reviews and contributed to the project
-          through pull requests, further enhancing my abilities in a dynamic team environment.
-        </p>
+        <div className='description '>
+          <p>
+            As a developer on a team developing a social media application, I had the pleasure of working on various
+            aspects of the project that worked together to create a rich and interactive experience for users. My
+            responsibilities included the development of cross-cutting functionalities such as sorting, filtering,
+            pagination and event handling, which were crucial not only for the administration panel, but for the entire
+            application, including the forum section, blog and other interactive modules.
+          </p>
+
+          <p>
+            With particular emphasis on the reusability of components, I built modals used not only for basic
+            administrative activities, but also for advanced operations, such as editing comments. These modals were
+            designed in a way that allows them to be easily customized and reused in different parts of the application,
+            which significantly accelerated the development of the project.{' '}
+          </p>
+
+          <p>
+            Integration with the API was implemented using React Query (RQ), which allowed for effective management of
+            the state of data coming from the server and optimization of the number of queries. This allows users to
+            enjoy a smooth and responsive application that updates data in real time without overloading the server.
+          </p>
+
+          <p>
+            {' '}
+            I used the Yup library to validate forms, which allowed me to create consistent and well-thought-out
+            validation schemes. Yup allowed for the rapid implementation of complex validation rules, which in turn
+            contributed to improving the quality of data entered by users and administrators.
+          </p>
+
+          <p>
+            Working closely with the team, gaining valuable experience and improving my Git version control skills were
+            key elements of my role. Constructive code reviews and active participation in projects through pull
+            requests have significantly improved my skills in a dynamic team environment. Through this process, I not
+            only improved the quality of my own code, but also contributed to the development of the project, which
+            allowed me to gain a deeper understanding of software development processes at scale.
+          </p>
+        </div>
       </div>
     </section>
   );
